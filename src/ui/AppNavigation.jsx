ea26cb6dd115dbf68bce51e9084c06f2b313d8eb
InezaHCl <inezaherve8@gmail.com>
2025-01-22 11:00:27 +0530

    add number of items in cart in AppNavigation

--- src/ui/AppNavigation.jsx
+++ src/ui/AppNavigation.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router";
 import { House, List, ShoppingCart, NotepadText } from "lucide-react";
+import { getTotalCartQuantity } from "../features/cart/cartSlice";
+import { useSelector } from "react-redux";
 
 export default function AppNavigation() {
+  const currentItemsCart = useSelector(getTotalCartQuantity);
+
   return (
     <div className="border-t border-stone-300 px-4 py-3">
       <ul className="flex items-center justify-between lg:justify-evenly">
@@ -39,11 +43,24 @@ export default function AppNavigation() {
             to="/app/cart"
             className="flex flex-col items-center text-sm hover:cursor-pointer hover:transition-all hover:duration-300"
           >
-            <ShoppingCart
+            {/* <ShoppingCart
               className="rounded-full bg-orange-600 p-3 text-white hover:bg-orange-700"
               width={50}
               height={50}
             />
+            Cart */}
+            <div className="relative">
+              <ShoppingCart
+                className="rounded-full bg-orange-600 p-3 text-white hover:bg-orange-700"
+                width={50}
+                height={50}
+              />
+              {currentItemsCart > 0 && (
+                <span className="absolute -top-3 right-0 flex h-6 w-6 items-center justify-center rounded-full border border-orange-600 bg-white text-xs font-bold text-orange-500 shadow-inner">
+                  {currentItemsCart}
+                </span>
+              )}
+            </div>
             Cart
           </Link>
         </li>
