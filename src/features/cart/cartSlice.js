830fae3f60ef33c20df3742d834f57b0ff93b978
InezaHCl <inezaherve8@gmail.com>
2025-01-22 12:36:47 +0530

    fix issue in store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,18 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //   cart: [],
+  cart: [],
 
   // For development purpose
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: "Mediterranean",
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
 };
 
 const cartSlice = createSlice({
