aea2909e0f825e13ebd630db4ca55d3f7cb5ec40
InezaHCl <inezaherve8@gmail.com>
2025-01-29 11:04:46 +0530

    add submit order functionality

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -11,7 +11,7 @@ export default function Cart() {
   const cart = useSelector(getCart);
 
   const dispatch = useDispatch();
-  console.log(cart);
+  // console.log(cart);
 
   if (cart.length <= 0) return <EmptyCart />;
 
