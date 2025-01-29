166f3c8509b78221fe86ef1b50dc8f30af26e690
InezaHCl <inezaherve8@gmail.com>
2025-01-29 11:18:00 +0530

    fix Layout spacing and Error component

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useRouteError } from "react-router";
-import Button from "./Button";
+import LinkButton from "./LinkButton";
 
 export default function Error() {
   const error = useRouteError();
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center">
       <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
-      <Button to="-1">&larr; Go back</Button>
+      <p className="mb-4">{error.data || error.message}</p>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 }
