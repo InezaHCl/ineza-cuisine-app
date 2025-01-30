1db329952824aed2e3ed50c5d0bd50d4a96e767b
InezaHCl <inezaherve8@gmail.com>
2025-01-30 11:16:49 +0530

    fix issue in address input placeholder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -77,7 +77,7 @@ export default function CreateOrder() {
               type="text"
               name="address"
               disabled={isLoadingAddress}
-              placeholder={`${isLoadingAddress && "Fetching address..."}`}
+              placeholder={`${isLoadingAddress ? "Fetching address..." : ""}`}
               defaultValue={address}
               required
               className="input w-full"
